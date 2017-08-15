a58794fe8efff5de99252bc3dadd49856425e94e
Ates Goral <ates.g@myplanet.com>
2017-08-15 02:45:27 -0400

    Fetch Dweet via proxy, Dweet runtime

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const request = require('request-promise-native');
+const cheerio = require('cheerio');
+const esprima = require('esprima');
+const estraverse = require('estraverse');
+
+const app = express();
+
+app.get('/api/dweets/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  request(`https://dweet.dwitter.net/id/${id}`)
+    .then((response) => {
+      // @todo error checking
+      const $ = cheerio.load(response);
+      const script = $('body script').html();
+      const ast = esprima.parseScript(script, { range: true });
+
+      let u = null;
+
+      estraverse.traverse(ast, {
+        enter: function (node) {
+          if (node.type === 'FunctionDeclaration' && node.id && node.id.name === 'u') {
+            u = script.slice.apply(script, node.range);
+            this.break();
+          }
+        }
+      });
+
+      if (u) {
+        res.type('application/javascript');
+        res.send(u);
+      } else {
+        // @todo
+      }
+    })
+    .catch(next);
+});
+
+app.use(express.static('./src/static'));
+
+const port = parseInt(process.env.port || 7890, 10);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ src/static/runtime.js
@@ -0,0 +1,52 @@
+var c = document.querySelector("#c");
+
+c.width = 1920;
+c.height = 1080;
+
+var S = Math.sin;
+var C = Math.cos;
+var T = Math.tan;
+
+function R(r,g,b,a) {
+  a = a === undefined ? 1 : a;
+  return "rgba("+(r|0)+","+(g|0)+","+(b|0)+","+a+")";
+};
+
+var x = c.getContext("2d");
+var time = 0;
+var frame = 0;
+
+let u = function () {};
+
+function loop() {
+  requestAnimationFrame(loop);
+
+  time = frame / 60;
+
+  if (time * 60 | 0 == frame - 1) {
+    time += 0.000001;
+  }
+
+  frame++;
+
+  try {
+    u(time);
+  } catch (e) {
+    throw e;
+  }
+}
+
+let id = 3066;
+
+function fetch(id) {
+  $.ajax(`/api/dweets/${id}`, { dataType: 'text' })
+    .then((response) => {
+      $.globalEval(`u = ${response}`);
+    })
+}
+
+$(() => {
+  fetch(id);
+
+  loop();
+});

3ba535a5946dafd5d7e611eb5d06a928ce56034f
Ates Goral <ates.g@myplanet.com>
2017-08-20 21:57:26 -0400

    Dweet cache

--- src/static/runtime.js
+++ src/static/runtime.js
@@ -3,6 +3,14 @@
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
+  const dweetCache = {
+    '701': { id: 701, user: 'sigveseb', src: '(F=Z=>{for(x.fillStyle=R(W=1/Z*4e3,W/2,W/4),i=Z*Z*2;n=i%Z,m=i/Z|0,i--;n%2^m%2&&x.fillRect((n-t%2-1)*W,(S(t)+m-1)*W,W,W));Z&&F(Z-6)})(36)//rm' },
+    '888': { id: 888, user: 'jczimm', src: 'c.width=1920;for(i=0;i<300;i++)for(j=0;j<6;j++){x.fillRect(960+200*C(i)*S(T(t\/1.1)+j\/i),540+200*S(i),10,10)}' },
+    '1231': { id: 1231, user: 'iverjo', src: 'c.width^=0;for(i=9;i<2e3;i+=2)s=3\/(9.1-(t+i\/99)%9),x.beginPath(),j=i*7+S(i*4+t+S(t)),x.lineWidth=s*s,x.arc(960,540,s*49,j,j+.6),x.stroke()' },
+    '739': { id: 739, user: 'donbright', src: 'c.width=900;\u534A=450;for(i=0.0;i<360;i+=1){x.lineTo(\u534A+C(i*t\/10)*i,\u534A*9\/16+S(i*t\/10)*i)};x.stroke();' },
+    '933': { id: 933, user: 'p01', src: 'for(d=2e3;d--;x.fillRect(960+d*C(a),540+d*S(a),24,24))a=Math.random()*6.3,x.fillStyle=R(e=255*C(t-1e3\/d*S(t-a-C(a*99\/d))),99*S(a-e\/d),6e4\/d)' }
+  };
+
   // @todo pass in as arg to IIFE
   function createRuntime() {
     var $ = undefined; // Hide jQuery
@@ -176,31 +184,22 @@
         return arguments[0];
       }
 
-      // let topDweetIds = [ 701, 888, 1231, 739, 933, 676, 855, 683, 1829, 697, 433, 135 ];
+      // let dweetIds = [ 701, 888, 1231, 739, 933, 676, 855, 683, 1829, 697, 433, 135 ];
+      let dweetIds = [ 701, 888, 1231, 739, 933 ];
 
-      // const fetches = topDweetIds
-      //   .sort(function () { return Math.random() - 0.5; })
-      //   .slice(0, 3)
-      //   .map(function (id, idx) {
-      //     return $.ajax(`/api/dweets/${id}`, { dataType: 'text' })
-      //       .then(createRuntime)
-      //       .then(progress);
-      //   });
+      const fetches = dweetIds
+        // .sort(function () { return Math.random() - 0.5; })
+        // .slice(0, 3)
+        .map((id, idx) => {
+          const cached = dweetCache[id];
+          const fetch = cached
+            ? pause(100 * idx).then(() => cached)
+            : $.ajax(`/api/dweets/${id}`, { dataType: 'json' });
 
-      const hardCodedDweets = [
-        { id: 701, user: 'sigveseb', src: '(F=Z=>{for(x.fillStyle=R(W=1/Z*4e3,W/2,W/4),i=Z*Z*2;n=i%Z,m=i/Z|0,i--;n%2^m%2&&x.fillRect((n-t%2-1)*W,(S(t)+m-1)*W,W,W));Z&&F(Z-6)})(36)//rm' },
-        { id: 888, user: 'jczimm', src: 'c.width=1920;for(i=0;i<300;i++)for(j=0;j<6;j++){x.fillRect(960+200*C(i)*S(T(t\/1.1)+j\/i),540+200*S(i),10,10)}' },
-        { id: 1231, user: 'iverjo', src: 'c.width^=0;for(i=9;i<2e3;i+=2)s=3\/(9.1-(t+i\/99)%9),x.beginPath(),j=i*7+S(i*4+t+S(t)),x.lineWidth=s*s,x.arc(960,540,s*49,j,j+.6),x.stroke()' },
-        { id: 739, user: 'donbright', src: 'c.width=900;\u534A=450;for(i=0.0;i<360;i+=1){x.lineTo(\u534A+C(i*t\/10)*i,\u534A*9\/16+S(i*t\/10)*i)};x.stroke();' },
-        { id: 933, user: 'p01', src: 'for(d=2e3;d--;x.fillRect(960+d*C(a),540+d*S(a),24,24))a=Math.random()*6.3,x.fillStyle=R(e=255*C(t-1e3\/d*S(t-a-C(a*99\/d))),99*S(a-e\/d),6e4\/d)' }
-      ];
-
-      const fetches = hardCodedDweets.map((dweet, i) => {
-        return pause(100 * i)
-          .then(() => dweet)
-          .then(createRuntime)
-          .then(progress);
-      });
+          return fetch
+            .then(createRuntime)
+            .then(progress);
+        });
 
       pending = fetches.length;
 

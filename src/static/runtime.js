f09656afff6c66bab329f68e58a2b86dbde1aa89
Ates Goral <ates.g@myplanet.com>
2017-08-20 21:28:55 -0400

    Get canvas with promise

--- src/static/runtime.js
+++ src/static/runtime.js
@@ -122,108 +122,110 @@
     }
   };
 
-  // @todo make this a promise as well
-  $(() => {
-    let renderer = createRuntime({ src: renderProgress.toString().split("'--marker--';")[1] });
-    let frameAdvancer = progressFrameAdvancer;
-    let blender = overwriteBlender;
+  function getCanvas() {
+    return new Promise((resolve) => $(() => resolve($("#c").get(0))));
+  }
 
-    const canvas = $("#c").get(0);
+  getCanvas()
+    .then((canvas) => {
+      let renderer = createRuntime({ src: renderProgress.toString().split("'--marker--';")[1] });
+      let frameAdvancer = progressFrameAdvancer;
+      let blender = overwriteBlender;
 
-    canvas.width = 1920;
-    canvas.height = 1080;
+      canvas.width = 1920;
+      canvas.height = 1080;
 
-    const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext('2d');
 
-    function render() {
-      requestAnimationFrame(render);
+      function render() {
+        requestAnimationFrame(render);
 
-      //const renderer = renderers[(frameCount / 100 | 0) % renderers.length];
+        //const renderer = renderers[(frameCount / 100 | 0) % renderers.length];
 
-      renderer.setFrame(frameAdvancer.getFrame());
+        renderer.setFrame(frameAdvancer.getFrame());
 
-      try {
-        renderer.render();
+        try {
+          renderer.render();
 
-        blender.beforeDraw(ctx);
+          blender.beforeDraw(ctx);
 
-        ctx.drawImage(
-          renderer.canvas,
-          0, 0, renderer.canvas.width, renderer.canvas.width * 1080 / 1920,
-          0, 0, canvas.width, canvas.height
-        );
-      } catch (e) {
-        console.error(e);
+          ctx.drawImage(
+            renderer.canvas,
+            0, 0, renderer.canvas.width, renderer.canvas.width * 1080 / 1920,
+            0, 0, canvas.width, canvas.height
+          );
+        } catch (e) {
+          console.error(e);
+        }
       }
-    }
 
-    function setStatus(tpl, params) {
-      $('#status').html(tpl.replace(/\$\{(.+?)\}/g, (s, name) => params[name]));
-    }
+      function setStatus(tpl, params) {
+        $('#status').html(tpl.replace(/\$\{(.+?)\}/g, (s, name) => params[name]));
+      }
 
-    function setDweetInfo(id, user) {
-      setStatus($('#dweet-info-tpl').html(), { id, user });
-    }
+      function setDweetInfo(id, user) {
+        setStatus($('#dweet-info-tpl').html(), { id, user });
+      }
 
-    render();
+      render();
 
-    let dweetRenderers = [];
-    let pending = 0;
+      let dweetRenderers = [];
+      let pending = 0;
 
-    function progress() {
-      progressFrameAdvancer.updateProgress(--pending, fetches.length);
-      return arguments[0];
-    }
+      function progress() {
+        progressFrameAdvancer.updateProgress(--pending, fetches.length);
+        return arguments[0];
+      }
 
-    // let topDweetIds = [ 701, 888, 1231, 739, 933, 676, 855, 683, 1829, 697, 433, 135 ];
+      // let topDweetIds = [ 701, 888, 1231, 739, 933, 676, 855, 683, 1829, 697, 433, 135 ];
 
-    // const fetches = topDweetIds
-    //   .sort(function () { return Math.random() - 0.5; })
-    //   .slice(0, 3)
-    //   .map(function (id, idx) {
-    //     return $.ajax(`/api/dweets/${id}`, { dataType: 'text' })
-    //       .then(createRuntime)
-    //       .then(progress);
-    //   });
+      // const fetches = topDweetIds
+      //   .sort(function () { return Math.random() - 0.5; })
+      //   .slice(0, 3)
+      //   .map(function (id, idx) {
+      //     return $.ajax(`/api/dweets/${id}`, { dataType: 'text' })
+      //       .then(createRuntime)
+      //       .then(progress);
+      //   });
 
-    const hardCodedDweets = [
-      { id: 701, user: 'sigveseb', src: '(F=Z=>{for(x.fillStyle=R(W=1/Z*4e3,W/2,W/4),i=Z*Z*2;n=i%Z,m=i/Z|0,i--;n%2^m%2&&x.fillRect((n-t%2-1)*W,(S(t)+m-1)*W,W,W));Z&&F(Z-6)})(36)//rm' },
-      { id: 888, user: 'jczimm', src: 'c.width=1920;for(i=0;i<300;i++)for(j=0;j<6;j++){x.fillRect(960+200*C(i)*S(T(t\/1.1)+j\/i),540+200*S(i),10,10)}' },
-      { id: 1231, user: 'iverjo', src: 'c.width^=0;for(i=9;i<2e3;i+=2)s=3\/(9.1-(t+i\/99)%9),x.beginPath(),j=i*7+S(i*4+t+S(t)),x.lineWidth=s*s,x.arc(960,540,s*49,j,j+.6),x.stroke()' },
-      { id: 739, user: 'donbright', src: 'c.width=900;\u534A=450;for(i=0.0;i<360;i+=1){x.lineTo(\u534A+C(i*t\/10)*i,\u534A*9\/16+S(i*t\/10)*i)};x.stroke();' },
-      { id: 933, user: 'p01', src: 'for(d=2e3;d--;x.fillRect(960+d*C(a),540+d*S(a),24,24))a=Math.random()*6.3,x.fillStyle=R(e=255*C(t-1e3\/d*S(t-a-C(a*99\/d))),99*S(a-e\/d),6e4\/d)' }
-    ];
+      const hardCodedDweets = [
+        { id: 701, user: 'sigveseb', src: '(F=Z=>{for(x.fillStyle=R(W=1/Z*4e3,W/2,W/4),i=Z*Z*2;n=i%Z,m=i/Z|0,i--;n%2^m%2&&x.fillRect((n-t%2-1)*W,(S(t)+m-1)*W,W,W));Z&&F(Z-6)})(36)//rm' },
+        { id: 888, user: 'jczimm', src: 'c.width=1920;for(i=0;i<300;i++)for(j=0;j<6;j++){x.fillRect(960+200*C(i)*S(T(t\/1.1)+j\/i),540+200*S(i),10,10)}' },
+        { id: 1231, user: 'iverjo', src: 'c.width^=0;for(i=9;i<2e3;i+=2)s=3\/(9.1-(t+i\/99)%9),x.beginPath(),j=i*7+S(i*4+t+S(t)),x.lineWidth=s*s,x.arc(960,540,s*49,j,j+.6),x.stroke()' },
+        { id: 739, user: 'donbright', src: 'c.width=900;\u534A=450;for(i=0.0;i<360;i+=1){x.lineTo(\u534A+C(i*t\/10)*i,\u534A*9\/16+S(i*t\/10)*i)};x.stroke();' },
+        { id: 933, user: 'p01', src: 'for(d=2e3;d--;x.fillRect(960+d*C(a),540+d*S(a),24,24))a=Math.random()*6.3,x.fillStyle=R(e=255*C(t-1e3\/d*S(t-a-C(a*99\/d))),99*S(a-e\/d),6e4\/d)' }
+      ];
 
-    const fetches = hardCodedDweets.map((dweet, i) => {
-      return pause(100 * i)
-        .then(() => dweet)
-        .then(createRuntime)
-        .then(progress);
-    });
+      const fetches = hardCodedDweets.map((dweet, i) => {
+        return pause(100 * i)
+          .then(() => dweet)
+          .then(createRuntime)
+          .then(progress);
+      });
 
-    pending = fetches.length;
+      pending = fetches.length;
 
-    Promise.all(fetches)
-      .then((_dweetRenderers) => {
-        dweetRenderers = _dweetRenderers;
-      })
-      .then(() => pause(1000))
-      .then(() => {
-        let dweetIdx = 0;
+      Promise.all(fetches)
+        .then((_dweetRenderers) => {
+          dweetRenderers = _dweetRenderers;
+        })
+        .then(() => pause(1000))
+        .then(() => {
+          let dweetIdx = 0;
 
-        frameAdvancer = monotonousFrameAdvancer;
-        renderer = dweetRenderers[dweetIdx];
-        setDweetInfo(renderer.id, renderer.user);
-        blender = fadeOutToWhiteBlender.reset();
-
-        setInterval(() => {
-          dweetIdx = (dweetIdx + 1) % dweetRenderers.length;
-          frameAdvancer = sineFrameAdvancer;
+          frameAdvancer = monotonousFrameAdvancer;
           renderer = dweetRenderers[dweetIdx];
           setDweetInfo(renderer.id, renderer.user);
-          // blender = fadeBlender.reset();
-          blender = overwriteBlender;
-        }, 5000);
-      });
-  });
+          blender = fadeOutToWhiteBlender.reset();
+
+          setInterval(() => {
+            dweetIdx = (dweetIdx + 1) % dweetRenderers.length;
+            frameAdvancer = sineFrameAdvancer;
+            renderer = dweetRenderers[dweetIdx];
+            setDweetInfo(renderer.id, renderer.user);
+            // blender = fadeBlender.reset();
+            blender = overwriteBlender;
+          }, 5000);
+        });
+    });
 })();
